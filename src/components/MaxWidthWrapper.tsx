d4fe5c32283f95dd5d1c0e15be2144e452543a0c
developmentd420 <development.d420@gmail.com>
2024-05-07 12:53:17 +0600

    Design hero section

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+interface IProps {
+  className?: string;
+  children: ReactNode;
+}
+
+const MaxWidthWrapper = ({ className, children }: IProps) => {
+  return (
+    <div
+      className={cn(
+        "h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default MaxWidthWrapper;
