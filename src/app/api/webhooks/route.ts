65d0146bdde7fd2c339e72aa5a3d7f9f54992be2
developmentd420 <development.d420@gmail.com>
2024-05-22 23:33:16 +0600

    debug stripe issue

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -39,6 +39,8 @@ export async function POST(req: Request) {
         throw new Error("Invalid request metadata");
       }
 
+      console.log("stripe session", session);
+
       const billingAddress = session.customer_details!.address;
       const shippingAddress = session.shipping_details!.address;
 
